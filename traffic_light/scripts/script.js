75fb7664e430855641476fc47e9d6c95451f3ce8
ivansamoilov <vanekk97@gmail.com>
2022-06-12 17:44:51 +0300

    traffic light

---
+++ traffic_light/scripts/script.js
@@ -0,0 +1,11 @@
+const circles = document.getElementsByClassName(`circle`);
+const button = document.getElementById(`btn`);
+
+const modificators = [`circle--red`, `circle--yellow`, `circle--green`];
+let currentPos = 0;
+
+const onClickNext = () => {  
+    circles[currentPos].classList.remove(modificators[currentPos]);  
+    currentPos !== 2 ? currentPos++ : currentPos = 0;
+    circles[currentPos].classList.add(modificators[currentPos])  
+}
